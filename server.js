e3107f79f2c6a46bfe17631c904fca40db92e25d
Gren-95 <fossfrog@protonmail.com>
2025-04-14 17:51:30 +0300

    change port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 3066;
 
 // Check for required environment variables
 if (!process.env.JWT_SECRET) {
