c515bbfefd4ce497511bec9f2d4896a176acda94
Gren-95 <fossfrog@protonmail.com>
2025-06-04 20:55:54 +0300

    changes frontend port to 7000

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 7000,
     strictPort: false, // Allow fallback to next available port
     host: true, // Listen on all local IPs
     open: true, // Automatically open the browser
